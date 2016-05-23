8763716e82af1f0c0ce80b8274cadfc6a60c5817
Sharrell Porter <SharrellPorter@example.com>
2016-05-23 05:40:13 -0400

    adding destroy ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  respond_to :html, :js
+
   def create
     @user = User.find(params[:user_id])
     @item = current_user.items.create(item_params)
@@ -8,6 +10,12 @@ class ItemsController < ApplicationController
     @user = User.find(params[:user_id])
     @item = Item.find(params[:id])
     @item.destroy
+
+    if @item.destroy
+      flash[:notice] = "Item was deleted successfully."
+    else
+      flash[:alert] = "Item couldn't be deleted. Try again."
+    end
   end
 
   private
