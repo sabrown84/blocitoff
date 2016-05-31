b89a8367f972863a83d810953d574f4670fd7a15
Sharrell Porter <SharrellPorter@example.com>
2016-05-31 14:36:21 -0400

    added distance of time helper method

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,7 @@
 module ItemsHelper
+  include ActionView::Helpers::DateHelper
+
+  def days_left(item)
+    distance_of_time_in_words(item.created_at, Date.today - 7.days)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ActiveRecord::Base
   validates :name, length: {minimum: 5}, presence: true
 
   default_scope { order('updated_at DESC') }
+
+  def days_left
+    distance_of_time_in_words(created_at, Date.today - 7.days)
+  end
 end
